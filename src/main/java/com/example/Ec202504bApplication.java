90ff0baffd9aa5ecd3e87e89ac23991b8bea808b
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-06-17 15:08:31 +0900

    再度感y環境作成しました。

---
+++ src/main/java/com/example/Ec202504bApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202504bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202504bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ec202504bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec202504bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
