125807196540f51006f0ca08f192f698e16d443b
Sota-Akahane <sota258@docomo.ne.jp>
2025-06-19 14:25:49 +0900

    商品一覧を表示&商品検索機能を実装。 商品の属性に関するDDL,DML文書を追加。

---
+++ src/main/java/com/example/controller/ShowItemListController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import com.example.domain.Item;
+import com.example.service.ShowItemListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * 商品関連機能の制御を行うコントローラクラス.
+ */
+@Controller
+@RequestMapping("/")
+public class ShowItemListController {
+
+    @Autowired
+    private ShowItemListService showItemListService;
+
+    @GetMapping("/showItemList")
+    public String searchAll(Model model) {
+        List<Item> itemList = showItemListService.searchAll();
+        model.addAttribute("itemList", itemList);
+
+        return "item_list_pizza";
+    }
+
+    @PostMapping("/searchByName")
+    public String searchByName(String name, Model model) {
+        List<Item> itemList = showItemListService.findByName(name);
+        model.addAttribute("itemList", itemList);
+
+        return "item_list_pizza";
+    }
+}
