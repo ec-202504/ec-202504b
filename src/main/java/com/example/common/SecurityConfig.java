51689bb3884596ab452768c61725afac5e424f85
suzukikoei <koeich6@gmail.com>
2025-06-20 11:44:50 +0900

    ログアウト機能を実装

--- src/main/java/com/example/common/SecurityConfig.java
+++ src/main/java/com/example/common/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,8 +29,9 @@ public class SecurityConfig {
                         .passwordParameter("password")
                 )
                 .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
+//                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout**"))
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/")
                         .deleteCookies("JSESSIONID")
                         .invalidateHttpSession(true)
                 )
